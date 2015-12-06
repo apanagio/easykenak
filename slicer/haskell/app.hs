af62fb9f1bd37ec407cfb0d25606c7a09a0ec963
apanagio <apanagio@gmail.com>
2015-12-06 13:35:44 +0200

    added haskell json input

--- slicer/haskell/app.hs
+++ slicer/haskell/app.hs
@@ -1,51 +1,34 @@
--- This example is basically the same as in Simplest.hs, only it uses
--- GHC's builtin generics instead of explicit instances of ToJSON and
--- FromJSON.
+{-# LANGUAGE OverloadedStrings,TemplateHaskell #-}
 
--- We enable the DeriveGeneric language extension so that GHC can
--- automatically derive the Generic class for us.
-
-{-# LANGUAGE DeriveGeneric #-}
-
-{-# LANGUAGE OverloadedStrings #-}
-
-import Data.Aeson (FromJSON, ToJSON, decode, encode, eitherDecode)
+import Data.Aeson
+import Data.Aeson.TH 
+--import qualified Data.Map as Map
+-- import Data.Maybe
 import qualified Data.ByteString.Lazy.Char8 as BL
+--import GHC.Generics
 
--- To decode or encode a value using the generic machinery, we must
--- make the type an instance of the Generic class.
-
-import GHC.Generics (Generic)
-
-data Obstacle = Obstacle {
-    offset:: [Double]
-    , height:: Double
-    , obstacle_geom:: [Double]
-    } deriving (Generic, Show)
-    
-data Edge = Edge {
-    edge_geom:: [Double]
-    , obstacles:: [Obstacle]
-    } deriving (Generic, Show)
 
 data Building = Building {
-    name ::          String
-    , orientation :: Int
-    , edges ::       [Edge]
-    } deriving (Generic, Show)
-
--- While we still have to declare our type as instances of FromJSON
--- and ToJSON, we do *not* need to provide bodies for the instances.
--- Default versions will be supplied for us.
+	name :: String
+	, orientation :: Float
+	, area :: Maybe Float
+	} deriving (Show, Eq)
 
 
-instance FromJSON Building
-instance ToJSON Building
+$(deriveJSON defaultOptions ''Building)
+
+-- slice :: Building -> a
+slice b = encode b
 
 main :: IO ()
 main = do
   
   building <- BL.getContents
   
-  let b = eitherDecode building :: Either String Building
-  print b  
+  let b = eitherDecode building :: Either String Building 
+  case b of 
+    Left err -> putStrLn err
+    Right d -> print $ slice d
+  
+  --putStrLn (getResult b)
+ 
