27d69d6a4fe95e9934beb005025b8801943454e0
apanagio <apanagio@gmail.com>
2015-12-06 21:08:36 +0200

    better data types

--- slicer/haskell/app.hs
+++ slicer/haskell/app.hs
@@ -2,23 +2,38 @@
 
 import Data.Aeson
 import Data.Aeson.TH 
+import Data.Aeson.Encode.Pretty
+
 --import qualified Data.Map as Map
 -- import Data.Maybe
 import qualified Data.ByteString.Lazy.Char8 as BL
---import GHC.Generics
 
+data Edge = Edge {
+	geom :: (Float, Float)
+	, obstacles :: Maybe [Object]
+	, specialObstacles :: Maybe Float
+	, diafani :: Maybe [Object]
+	, adiafani :: Maybe [Object]
+	, levels :: Maybe [Object]
+	}
 
 data Building = Building {
 	name :: String
 	, orientation :: Float
-	, area :: Maybe Float
-	} deriving (Show, Eq)
+	, adiafaniType :: Maybe String
+	, height :: Maybe Float
+	, heightNet :: Maybe Float
+	, edges :: [Edge]
+	, balconies :: Maybe [Object]
+	, tents :: Maybe [String]
+	, epafes :: Maybe [String]
+	}
 
-
-$(deriveJSON defaultOptions ''Building)
+deriveJSON defaultOptions ''Edge
+deriveJSON defaultOptions ''Building
 
 -- slice :: Building -> a
-slice b = encode b
+slice b = encodePretty $ edges b
 
 main :: IO ()
 main = do
@@ -28,7 +43,6 @@ main = do
   let b = eitherDecode building :: Either String Building 
   case b of 
     Left err -> putStrLn err
-    Right d -> print $ slice d
+    Right d -> BL.putStrLn $ slice d
   
-  --putStrLn (getResult b)
  
