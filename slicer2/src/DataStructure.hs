c89acc4158e3c17d746e243e39d5ea0b5f21853a
apanagio <apanagio@gmail.com>
2016-06-02 20:26:59 +0300

    first building

--- slicer2/src/DataStructure.hs
+++ slicer2/src/DataStructure.hs
@@ -3,7 +3,7 @@
 module DataStructure where
 
 import Data.Aeson
-import Data.Aeson.TH 
+import Data.Aeson.TH
 
 import Data.Char(toLower)
 
@@ -13,12 +13,12 @@ import Algebra
 type OnEdge = Double
 type OnSkel = (Int, OnEdge)
 
-data Epafi = Epafi {
-  epfType :: Int
-  , epfStart :: OnSkel
-  , epfEnd :: OnSkel
-  , epfStartHeight :: Interval
-  , epfEndHeight :: Interval
+data Item a = Item {
+  start :: OnSkel
+  , end :: OnSkel
+  , startHeight :: Interval
+  , endHeight :: Interval
+  , props :: a
 } deriving (Eq, Show)
 
 data Obstacle = Obstacle {
@@ -34,34 +34,27 @@ data Balcony = Balcony {
   , balcGeom :: [Vec]
 } deriving (Eq, Show)
 
-data Item a = Item {
-  fromTo :: Interval
-  , startHeight :: Interval
-  , endHeight :: Interval
-  , props :: a
+data Edges = Edges {
+  geom :: [Vec]
+  , height :: [Double]
+  , specialObstacles :: [Double]
 } deriving (Eq, Show)
-
-data Edge = Edge {
-  geom :: Vec
-  , height :: Double
-  , specialObstacles :: Double
-  , diafani :: [Item String]
-  , adiafani :: [Item String]
-  , levels :: [Item String]
-  } deriving (Eq, Show)
 
 data Building = Building {
   orientation :: Double
   , adiafaniType :: String
   , heightNet :: Double
   , heightGross :: Double
-  , edges :: [Edge]
+  , edges :: Edges
   , balconies :: [Balcony]
   , tents :: [Balcony]
-  , epafes ::  [Epafi]
+  , epafes ::  [Item EpafiProps]
   , obstacles :: [Obstacle]
+  , diafani :: [Item String]
+  , adiafani :: [Item String]
+  , levels :: [Item String]
   } deriving (Eq, Show)
-  
+
 -- intermediate data
 data ObstacleProps = ObstacleProps {
   distance :: (Double, Double)
@@ -70,7 +63,7 @@ data ObstacleProps = ObstacleProps {
 
 data BalconyProps = BalconyProps {
   parsedBalcGeom :: [Vec]
-  , parsedBalcHeight :: Double 
+  , parsedBalcHeight :: Double
 } deriving Show
 
 type EpafiProps = Int
@@ -79,20 +72,19 @@ type ParsedObstacle = Item ObstacleProps
 type ParsedEpafi = Item EpafiProps
 type ParsedBalcony = Item BalconyProps
 
-data ParsedEdge = ParsedEdge {
-  edge :: Edge
-  , rank :: Int
-  , startPoint :: Vec
-  , len :: Double -- length of all edges before this one
-  , parsedObstacles :: [ParsedObstacle]
-  , parsedBalconies :: [ParsedBalcony]
-  , parsedTents :: [ParsedBalcony]
-  , parsedEpafes :: [ParsedEpafi]
-} deriving Show
+--   edge :: Edge
+--   , rank :: Int
+--   , startPoint :: Vec
+--   , len :: Double -- length of all edges before this one
+--   , parsedObstacles :: [ParsedObstacle]
+--   , parsedBalconies :: [ParsedBalcony]
+--   , parsedTents :: [ParsedBalcony]
+--   , parsedEpafes :: [ParsedEpafi]
+-- } deriving Show
+-- data ParsedEdge = ParsedEdge {
 
-$(deriveJSON defaultOptions ''Edge)
+$(deriveJSON defaultOptions ''Edges)
 $(deriveJSON defaultOptions ''Building)
 $(deriveJSON defaultOptions ''Item)
 $(deriveJSON defaultOptions{fieldLabelModifier = (\(x:xs) -> (toLower x:xs)) . drop 4 } ''Obstacle)
 $(deriveJSON defaultOptions{fieldLabelModifier = (\(x:xs) -> (toLower x:xs)) . drop 4 } ''Balcony)
-$(deriveJSON defaultOptions{fieldLabelModifier = (\(x:xs) -> (toLower x:xs)) . drop 3 } ''Epafi)
--- slicer2/src/Lib.hs
+++ slicer2/src/Lib.hs
@@ -1,8 +1,8 @@
-{-# LANGUAGE OverloadedStrings,TemplateHaskell #-}
--- {-# LANGUAGE OverloadedStrings #-}
+-- {-# LANGUAGE OverloadedStrings,TemplateHaskell #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module Lib
-    ( someFunc
+    ( run
     ) where
 
 
@@ -13,41 +13,25 @@ import qualified Data.ByteString.Lazy.Char8 as BL
 
 import DataStructure
 import Algebra
-import Utils
-import Shadows
-import Epafes
-import Balconies
+-- import Utils
+-- import Shadows
+-- import Epafes
+-- import Balconies
 
-import qualified Plotter as PL
+-- import qualified Plotter as PL
 
 --temp
 import qualified Data.Vector as V
 ---
 
-someFunc :: IO ()
-someFunc = do
+run :: IO ()
+run = do
   building <- BL.getContents
   let b = eitherDecode building :: Either String Building
   case b of
     Left err -> putStrLn err
     Right d -> do
-      writeFile "/tmp/ttt.dat" $ PL.plot d
-      let c = getParsedEdges $ edges d
-      let a = V.fromList c
-      -- ~ putStrLn $ show $ pointFromSkel (edges d) (4, 1.0)
-      -- ~ putStrLn $ show $ getEpafes d
-      -- ~ print $ map len $ getParsedEdges $ edges d
-      -- ~ print $ getShadowsFromEdge (a V.! 4) (getParsedEdges $ edges d)
-      -- ~ print $ obstFromEdge (a V.! 4) (a V.! 1)
-      -- ~ print $ getShadowsFromObst (a V.! 4) (obstacles d)
-      -- ~ print $ mergeShadows (heightGross d) $ getAllShadows (a V.! 4) d
-      -- ~ print $ getAllShadows (a V.! 4) d
-      -- ~ print $ epafiFromEdge (a V.! 0) (head $ epafes d)
-      -- print $ epafiFromEdge c (a V.! 2) (head $ epafes d)
-      -- print $ getMergedShadows d (a V.! 4)
-      -- ~ print $ getEpafes d (a V.! 1)
-      -- ~ print $ getBalcLines (edges d) $ head $ balconies d
-      print $ map (addEpafes d . addShadows d) c
-      -- ~ print $ getBalcDist (edges d) (head $ balconies d) (1, 0.7)
-      -- ~ print $ getEdgeWithBalc (edges d) (head $ balconies d)
-      putStrLn "OK"
+  --     writeFile "/tmp/ttt.dat" $ PL.plot d
+  --     let c = getParsedEdges $ edges d
+  --     let a = V.fromList c
+      print d
